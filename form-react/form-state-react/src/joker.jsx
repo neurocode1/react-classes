8778c4a09727299b6520ad7b2b081afcea6308d6
manish00072 <playconsole87@gmail.com>
2026-05-27 05:30:23 -0500

    change

--- form-react/form-state-react/src/joker.jsx
+++ form-react/form-state-react/src/joker.jsx
@@ -1,47 +1,103 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Joker(){
+import { useEffect, useState } from "react"
 
-    let [joke,setjoke]= useState({})
+// export default function Joker(){
 
-    const url = "https://official-joke-api.appspot.com/random_joke"
+//     let [joke,setjoke]= useState({})
+
+//     const url = "https://official-joke-api.appspot.com/random_joke"
 
     
-    const getjock = async()=>{
-        let response  = await fetch (url);
-        let josnreposne =  await response.json()
-        console.log(josnreposne)
-        setjoke( {setup: josnreposne.setup , punchline : josnreposne.punchline})
+//     const getjock = async()=>{
+//         let response  = await fetch (url);
+//         let josnreposne =  await response.json()
+//         console.log(josnreposne)
+//         setjoke( {setup: josnreposne.setup , punchline : josnreposne.punchline})
+//     }
+
+//     useEffect(()=>{
+//            async function getnewjoke(){
+//             let response  = await fetch (url);
+//         let josnreposne =  await response.json()
+//         console.log(josnreposne)
+//         setjoke( {setup: josnreposne.setup , punchline : josnreposne.punchline})
+//         } 
+
+// getnewjoke()
+
+//         }, []);
+
+
+//     return(
+//         <div>
+
+//          <h1>jocks</h1>  
+//          <h2>{joke.setup}</h2> 
+//          <h3>{joke.punchline}</h3>
+//          <button onClick={getjock}> get joke</button>
+
+    
+        
+        
+        
+// </div>
+        
+        
+        
+//     )
+// }
+
+
+
+
+
+export default  function joke(){
+
+    let [ joke,setjoke ]= useState({})
+
+
+   const url = "https://official-joke-api.appspot.com/random_joke"
+
+
+
+   const getjoke = async () => {
+    let Response = await fetch (url)
+   let  jsonreposne = await Response.json()
+   console.log (jsonreposne);
+   setjoke({setup : jsonreposne.setup , punchline : jsonreposne.punchline})
+   }
+
+
+
+   useEffect(()=>{
+    async function getnewjoke(){
+        let Response = await fetch (url)
+   let  jsonreposne = await Response.json()
+   console.log (jsonreposne);
+   setjoke({setup : jsonreposne.setup , punchline : jsonreposne.punchline})
+
     }
 
-    useEffect(()=>{
-           async function getnewjoke(){
-            let response  = await fetch (url);
-        let josnreposne =  await response.json()
-        console.log(josnreposne)
-        setjoke( {setup: josnreposne.setup , punchline : josnreposne.punchline})
-        } 
+    getjoke();
 
-getnewjoke()
+},[])
 
-        }, []);
+
+
+
 
 
     return(
         <div>
 
-         <h1>jocks</h1>  
-         <h2>{joke.setup}</h2> 
-         <h3>{joke.punchline}</h3>
-         <button onClick={getjock}> get joke</button>
+            <h1>{joke.setup}</h1>
+            <h2>{joke.punchline}</h2>
 
-    
-        
-        
-        
-</div>
-        
-        
-        
+            <button onClick={getjoke}>joke</button>
+
+
+
+        </div>
     )
 }
